a79efd5016686335819035a67a66e6538fc89365
nodirbekerkabayev <shaxzod@koryeogs.com>
2025-08-29 08:31:44 +0500

    bug fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,25 +20,5 @@ class DatabaseSeeder extends Seeder
             'email' => 'admin',
             'password' => bcrypt('1234')
         ]);
-        for ($i = 0; $i < 10; $i++) {
-            Client::create([
-                'name' => "Client name" . $i,
-                'info' => "Client info" . $i,
-                'phone' => "+99899765432" . $i,
-                'image' => "clients" . $i . ".jpg",
-                'debt' => $i . '00000',
-                'recorded_by' => ['jasur', 'nodira', 'hilola'][array_rand(['jasur', 'nodira', 'hilola'])],
-                'is_deleted' => $i % 2 === 0 ? true : false,
-            ]);
-        }
-        for ($i = 0; $i < 10; $i++) {
-            Debt::create([
-                'client_id' => $i+1,
-                'amount' => $i . '00000',
-                'status' => $i % 2 === 0 ? 'oldi' : 'berdi',
-                'recorded_by' => ['jasur', 'nodira', 'hilola'][array_rand(['jasur', 'nodira', 'hilola'])],
-                'is_deleted' => $i % 2 === 0 ? true : false,
-            ]);
-        }
     }
 }
